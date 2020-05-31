e5fcbc0dcabd849165220be6139aa54cce75f2c3
Bert <z0bristbr@gmail.com>
2020-05-30 22:05:27 -0300

    set up javascript and logo via CSS

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 
-function counterFunc () {
-
-// let count = getElementsByClassName('switch')
-console.log('Chamou!!!')
-
+function counterFunc (count) {
+    
+    if(count === 5369) {
+       document.getElementById("overlay").style.display = "block";
+    }
+    
+    return document.getElementById("counter").innerHTML = count;
+  
 }
