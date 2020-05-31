5fb6e8d656305e4cc65f2eb2de73121f2bd82d28
Bert <z0bristbr@gmail.com>
2020-05-31 13:56:50 -0300

    set eslint.rc and nodemon

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'htmal');
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+
+app.set('view engine', 'htmal')
 
 app.use('/', (req, res) => {
-    res.render('index.html');
+  res.render('index.html')
 })
 
-let count = [];
-//let count = 0;
+const count = []
+// let count = 0;
 
-io.on('connection', socket => {
-    console.log(`Socket conetado: ${socket.id}`);
+io.on('connection', (socket) => {
+  console.log(`Socket conetado: ${socket.id}`)
 
-    socket.emit('previousNumber', count.length);
+  socket.emit('previousNumber', count.length)
 
-    socket.on('plusLigth', data => {
-        count.push(data);
-        //count++;
-        //console.log(count.length)
-        let number = count.length;
-        //console.log(number)
-        socket.broadcast.emit('numberOfLigths', number);
-    });
+  socket.on('plusLigth', (data) => {
+    count.push(data)
+    // count++;
+    // console.log(count.length)
+    const number = count.length
+    // console.log(number)
+    socket.broadcast.emit('numberOfLigths', number)
+  })
 
-    socket.on('refresh', data => {
-        socket.broadcast.emit('reload', true);
-    });
+  socket.on('refresh', (data) => {
+    socket.broadcast.emit('reload', true)
+  })
 })
 
-server.listen(3000);
+server.listen(3000)
