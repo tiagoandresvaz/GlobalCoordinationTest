074219bbe66c3e1ecc2f5c9b7057ceab0d1c29f4
Bert <z0bristbr@gmail.com>
2020-05-31 12:53:35 -0300

    set EarthGlobe CSS effect

--- src/public/main.js
+++ src/public/main.js
@@ -2,6 +2,7 @@
 function counterFunc (count) {
     
     if(count === 5369) {
+       document.getElementById("earth").style.backgroundImage = 'url(./assets/earthNight.jpg)'
        document.getElementById("overlay").style.display = "block";
     }
     
